3f87678bd79b436f1d021465ed8cb525d59035e4
fernando <s24h@outlook.es>
2024-09-16 05:32:19 -0500

    2

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
+// next.config.mjs
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    output: 'export',
+    // Agrega cualquier otra configuración si es necesario
+  };
+  
+  export default nextConfig;
+  
